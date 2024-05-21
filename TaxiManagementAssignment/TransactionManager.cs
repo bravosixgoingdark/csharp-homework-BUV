7ff40c9c7f058dfb49f2fd2191693664db0827ca
captainprice <captainprice@DESKTOP-9UNTM96>
2024-05-21 14:31:04 +0700

    Add TransactionManager code

---
+++ TaxiManagementAssignment/TransactionManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiManagementAssignment
+{
+    public class TransactionManager
+    {
+        public TransactionManager() {
+            _transactions = new List<Transaction>();
+        }
+        private List<Transaction> _transactions;
+        public List<Transaction> transactions { get { return _transactions; } }
+        public void RecordJoin(int taxiNum, int rankID) {
+            DateTime dateTime = DateTime.Now;
+            Transaction transaction = new JoinTransaction(dateTime, taxiNum, rankID);
+            _transactions.Add(transaction);
+        }
+        public void RecordLeave(int rankID, Taxi taxi)
+        {
+            DateTime dateTime = DateTime.Now;
+            Transaction transaction = new LeaveTransaction(dateTime, rankID, taxi);
+            _transactions.Add(transaction);
+        }
+        public void RecordDrop(int taxiNum, bool pricePaid)
+        {
+            DateTime dateTime = DateTime.Now;
+            Transaction transaction = new DropTransaction(dateTime, taxiNum, pricePaid);
+            _transactions.Add(transaction);
+        }
+        public List<Transaction> GetAllTransactions() { return _transactions; }
+    }
+}
--- TaxiManagementTests/_06_TransactionManager_Tests.cs
+++ TaxiManagementTests/_06_TransactionManager_Tests.cs
@@ -3,34 +3,35 @@ using TaxiManagementAssignment;
 
 namespace TaxiManagementTests
 {
+    [TestClass]
     public class _06_TransactionManager_Tests
     {
         /*
          * Uncomment from line 13
          */
 
-        //[TestMethod]
-        //public void _01_RecordJoinCreatesJoinTransaction()
-        //{
-        //    TransactionManager tm = new TransactionManager();
-        //    tm.RecordJoin(1, 1);
-        //    Assert.AreEqual("JoinTransaction", tm.GetAllTransactions()[0].GetType().Name);
-        //}
+        [TestMethod]
+        public void _01_RecordJoinCreatesJoinTransaction()
+        {
+            TransactionManager tm = new TransactionManager();
+            tm.RecordJoin(1, 1);
+            Assert.AreEqual("JoinTransaction", tm.GetAllTransactions()[0].GetType().Name);
+        }
 
-        //[TestMethod]
-        //public void _02_RecordLeaveCreatesLeaveTransaction()
-        //{
-        //    TransactionManager tm = new TransactionManager();
-        //    tm.RecordLeave(1, new Taxi(1));
-        //    Assert.AreEqual("LeaveTransaction", tm.GetAllTransactions()[0].GetType().Name);
-        //}
+        [TestMethod]
+        public void _02_RecordLeaveCreatesLeaveTransaction()
+        {
+            TransactionManager tm = new TransactionManager();
+            tm.RecordLeave(1, new Taxi(1));
+            Assert.AreEqual("LeaveTransaction", tm.GetAllTransactions()[0].GetType().Name);
+        }
 
-        //[TestMethod]
-        //public void _03_RecordDropCreatesDropTransaction()
-        //{
-        //    TransactionManager tm = new TransactionManager();
-        //    tm.RecordDrop(1, true);
-        //    Assert.AreEqual("DropTransaction", tm.GetAllTransactions()[0].GetType().Name);
-        //}
+        [TestMethod]
+        public void _03_RecordDropCreatesDropTransaction()
+        {
+            TransactionManager tm = new TransactionManager();
+            tm.RecordDrop(1, true);
+            Assert.AreEqual("DropTransaction", tm.GetAllTransactions()[0].GetType().Name);
+        }
     }
 }
